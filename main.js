3696ae6028506549de0b3964979a99cf9e1e3245
KodPixel-X <hanifikorkmaz184@gmail.com>
2025-12-02 21:16:29 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
-input = prompt("Soruyu Yazınız. UYARI!!! ⚠️⚠️⚠️⚠️ Pqq-Ai yeni olduğundan çoğu soruları hatalı verir yada Hiç vermez: ");
+let input = prompt("Soruyu Yazınız. UYARI!!! ⚠️⚠️⚠️⚠️ Pqq-Ai yeni olduğundan çoğu soruları hatalı verir yada Hiç vermez: ");
 
 let veri = { 
     "Merhaba": "merhaba iyim sen"
+}
+while (true) {
+    if (!input) break;
+    input = input.toLowerCase();
+    
+   if (veri[input.toLowerCase()]) {
+    alert(veri[input.toLowerCase()]);
+} else {
+    alert("Üzgünüm, O Konuda Bilgim Yok.");
+
+}
+ 
 }
 
 if (veri[input.toLowerCase()]) {
     alert(veri[input.toLowerCase()]);
 } else {
     alert("Üzgünüm, O Konuda Bilgim Yok.");
+
 }
