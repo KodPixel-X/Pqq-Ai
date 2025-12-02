d3e9de6c499f20b16d361192b1e428dc12c3671e
KodPixel-X <hanifikorkmaz184@gmail.com>
2025-12-02 21:12:25 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+input = prompt("Soruyu Yazınız. UYARI!!! ⚠️⚠️⚠️⚠️ Pqq-Ai yeni olduğundan çoğu soruları hatalı verir yada Hiç vermez: ");
+
+let veri = { 
+    "Merhaba": "merhaba iyim sen"
+}
+
+if (veri[input.toLowerCase()]) {
+    alert(veri[input.toLowerCase()]);
+} else {
+    alert("Üzgünüm, O Konuda Bilgim Yok.");
+}
