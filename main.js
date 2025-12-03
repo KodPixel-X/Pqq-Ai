a8c47dcdfc28ee33c6c46eb5bf9b3cc5b1e33d12
KodPixel-X <hanifikorkmaz184@gmail.com>
2025-12-03 11:20:35 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 let input = prompt("Soruyu Yazınız. UYARI!!! ⚠️⚠️⚠️⚠️ Pqq-Ai yeni olduğundan çoğu soruları hatalı verir yada Hiç vermez: ");
 
-let veri = { 
-    "Merhaba": "merhaba iyim sen"
+if (input == "Merhaba"){
+    alert("Merhaba Nasılsın");
 }
-
-if (veri[input.toLowerCase()]) {
-    alert(veri[input.toLowerCase()]);
-} else {
-    alert("Üzgünüm, O Konuda Bilgim Yok.");
-
+if (input == "İyyim"){
+    alert("Senin İyi Olmana Sevindim Günün Nasıl Geçiyor");
 }
-
+if (input == "Günüm İyi Geçiyor"){
+    alert("Günün İyi Geçmesine Sevindim");
+}
