853e7206d182ee7e9bd42b733a561922702f57b1
KodPixel-X <hanifikorkmaz184@gmail.com>
2025-12-03 11:27:40 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
 let input = prompt("Soruyu Yazınız. UYARI!!! ⚠️⚠️⚠️⚠️ Pqq-Ai yeni olduğundan çoğu soruları hatalı verir yada Hiç vermez: ");
 
-if (input == "Merhaba"){
+while (true) {
+    if (input == "Merhaba"){
     alert("Merhaba Nasılsın");
-}
-if (input == "İyyim"){
+    }
+
+    if (input == "İyyim"){
     alert("Senin İyi Olmana Sevindim Günün Nasıl Geçiyor");
-}
-if (input == "Günüm İyi Geçiyor"){
+    }
+
+    if (input == "Günüm İyi Geçiyor"){
     alert("Günün İyi Geçmesine Sevindim");
+    }   
+
+    if (input == "Çık") {
+        alert("Görüşürüz");
+        break;
+    }  
+
+    if (input == "Sen Yerlimisin") {
+        alert("Evet Yerliyim Türkiye");
+    }
+    if (input == "Nerelisin")
+    {
+        alert("Türkiyeliyim");
+    }
 }
